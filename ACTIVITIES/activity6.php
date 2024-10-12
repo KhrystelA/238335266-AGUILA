6b0ffa5199564948b751dc67eee322ef24597aa4
KhrystelA <kda5266@students.uc-bcf.edu.ph>
2024-10-13 01:08:43 +0800

    Create activity6.php

---
+++ ACTIVITIES/activity6.php
@@ -0,0 +1,14 @@
+<?php
+// Activity 6: Array Iteration with foreach
+$favMovies = [
+    "The Shawshank Redemption",
+    "Inception",
+    "The Dark Knight",
+    "Interstellar",
+    "Parasite"
+];
+
+foreach ($favMovies as $movie) {
+    echo $movie . "<br>";
+}
+?>
