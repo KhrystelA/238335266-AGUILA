0793cd88ceacb965d7c491df09ce6d0f388f318d
KhrystelA <kda5266@students.uc-bcf.edu.ph>
2024-10-13 01:09:34 +0800

    Create activity8.php

---
+++ ACTIVITIES/activity8.php
@@ -0,0 +1,11 @@
+<?php
+// Activity 8: Factorial Calculator
+$number = 5; 
+$factorial = 1;
+
+for ($i = 1; $i <= $number; $i++) {
+    $factorial *= $i;
+}
+
+echo "Factorial of $number is: $factorial";
+?>
