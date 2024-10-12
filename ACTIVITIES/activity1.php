efb520cff71816a3d33384c85b077adbbc2406eb
KhrystelA <kda5266@students.uc-bcf.edu.ph>
2024-10-13 01:06:57 +0800

    Create activity1.php

---
+++ ACTIVITIES/activity1.php
@@ -0,0 +1,17 @@
+<?php
+// Activity 1: Number Counter
+// Printing numbers from 1 to 10 using a while loop
+$count = 1;
+while ($count <= 10) {
+    echo $count . " ";
+    $count++;
+}
+
+// Printing even numbers between 1 and 20
+echo "<br>Even numbers between 1 and 20:<br>";
+$count = 2;
+while ($count <= 20) {
+    echo $count . " ";
+    $count += 2;
+}
+?>
