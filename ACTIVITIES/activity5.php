5c981724a2b1d9e930f69ac13712763fa976cb5f
KhrystelA <kda5266@students.uc-bcf.edu.ph>
2024-10-13 01:08:25 +0800

    Create activity5.php

---
+++ ACTIVITIES/activity5.php
@@ -0,0 +1,12 @@
+<?php
+// Activity 5: Sum of Numbers
+$sum = 0;
+$count = 1;
+
+while ($count <= 100) {
+    $sum += $count;
+    $count++;
+}
+
+echo "The sum of numbers from 1 to 100 is: $sum";
+?>
