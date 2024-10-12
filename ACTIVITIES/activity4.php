926354a5b795b7d665c7887972565af303edaa89
KhrystelA <kda5266@students.uc-bcf.edu.ph>
2024-10-13 01:08:08 +0800

    Create activity4.php

---
+++ ACTIVITIES/activity4.php
@@ -0,0 +1,12 @@
+<?php
+// Activity 4: Loop Control with break and continue
+for ($i = 1; $i <= 10; $i++) {
+    if ($i == 5) {
+        continue; 
+    }
+    if ($i == 8) {
+        break; 
+    }
+    echo $i . " ";
+}
+?>
